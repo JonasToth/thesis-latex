5647c5f207220a536569d82e5415499462146a23
Jonas Toth <development@jonas-toth.eu>
2019-10-07 16:31:57 +0200

    improve section on flexion image, some meta stuff

--- build_local.sh
+++ build_local.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run  --rm \
-            --volume "$(pwd):/project" \
-            -u $(id -u):$(id -g) \
-            tubaf_latex
+docker run --rm \
+           --volume "$(pwd):/project" \
+           -u $(id -u):$(id -g) \
+           tubaf_latex main.tex
 
 rm main.bbl main.nlg main.nlo main.nls
