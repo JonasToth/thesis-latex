bb14583385d454582f2efc6c8924412281b22796
Jonas Toth <development@jonas-toth.eu>
2020-05-28 18:06:30 +0200

    [Misc] dont plot everything on every run now, because plots are done

--- build_local.sh
+++ build_local.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./copy_results.sh || exit 1
-cd chapter06/results/ || exit 1
-./produce_plots.sh || exit 1
-./produce_tables.sh || exit 1
-cd ../../ || exit 1
+# ./copy_results.sh || exit 1
+# cd chapter06/results/ || exit 1
+# ./produce_plots.sh || exit 1
+# ./produce_tables.sh || exit 1
+# cd ../../ || exit 1
 
 docker run --rm \
            --volume "$(pwd):/project" \
