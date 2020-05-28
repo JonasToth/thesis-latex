7dd203e02dd54599997908bd287ada660d3c2378
Jonas Toth <development@jonas-toth.eu>
2020-05-28 17:00:17 +0200

    [Fix] new latex version and tubaf latex, adjust some stuff already mentioned, fix up positioning for some parts due to new bold face

--- build_local.sh
+++ build_local.sh
@@ -9,6 +9,4 @@ cd ../../ || exit 1
 docker run --rm \
            --volume "$(pwd):/project" \
            -u $(id -u):$(id -g) \
-           masterarbeit_latex main.tex
-
-rm main.bbl main.nlg main.nlo main.nls
+           masterarbeit_latex2 main.tex
